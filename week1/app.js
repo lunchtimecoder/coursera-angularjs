c19f85399958ad2871ca365b05b3945f7c54a817
markpateman <email@lunchtime.com>
2016-11-24 22:58:21 +0000

    AngularJS Week 1 First code

--- week1/app.js
+++ week1/app.js
@@ -5,22 +5,22 @@
       {
         //init vars
         $scope.items = "";  //will hold items to process
-        $scope.basket = {"message":"","colour":"boxColorBlack","note":""}; //return object
+        $scope.basket = { "message":"", "colour":"boxColorBlack", "note":"" }; //return object
         $scope.displayMsg = function () {
           $scope.basket = processItems($scope.items)
         };
 
         function processItems(inputItems) {
           var itemsArray = inputItems.split(",");
-          var rtn = {"message":"Please enter data first","colour":"boxColorBlack","note":""};
+          var rtn = { "message":"Please enter data first", "colour":"boxColorBlack", "note":""};
 
           //Test if no items
           if(inputItems != "") {
             //Test if less than or equal to 3 items
             if(itemsArray.length <= 3) {
-              rtn = {"message":"Enjoy!","colour":"boxColorBlue","note":""};
+              rtn = { "message":"Enjoy!", "colour":"boxColorBlue", "note":""};
             } else {
-              rtn = {"message":"Too Much!","colour":"boxColorRed","note":""};
+              rtn = { "message":"Too Much!", "colour":"boxColorRed", "note":""};
             }
             //Check for any empty items
             if(itemsArray.filter(checkForEmpties).length == 0) { rtn.note = ""; }
