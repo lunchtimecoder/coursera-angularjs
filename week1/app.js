aeae8541252b723bc9a96e507630d1835322ce66
markpateman <email@lunchtime.com>
2016-11-24 22:13:36 +0000

    AngularJS Week 1 First code

--- week1/app.js
+++ week1/app.js
@@ -1,11 +1,36 @@
-/**
- * Created by markpateman on 22/11/2016.
- */
 (function () {
   'use strict'
-  angular.module('MyApp',[])
-      .controller('MyController', function ($scope) {
-  $scope.name = "Some Test Text";
-  });
+  angular.module("MyApp", [])
+      .controller("MyController", function ($scope)
+      {
+        $scope.items = "";
+        $scope.basket = {"message":"","colour":"boxColorBlack","note":""};
+        $scope.rtnColour = "boxColorBlack";
+        $scope.displayMsg = function () {
+          $scope.basket = procItems($scope.items)
+        };
 
-})();
+        function procItems(inputItems) {
+          var itemsArray = inputItems.split(",");
+          var itemsCount = itemsArray.length;
+          var rtn = {"message":"Please enter data first","colour":"boxColorBlack","note":""};
+          if(inputItems != "") {
+            if(itemsCount <= 3) {
+              rtn = {"message":"Enjoy!","colour":"boxColorBlue","note":""};
+            } else {
+              rtn = {"message":"Too Much!","colour":"boxColorRed","note":""};
+            }
+          }
+          rtn.note = checkForEmpties(itemsArray);
+          return rtn;
+        };
+
+        function checkForEmpties(itemsToCheck) {
+          var rtnNote = itemsToCheck;
+          for (var i in itemsToCheck) {
+            if(i.trim.length < 1 ) {rtnNote = "I do NOT consider an empty item valid food!";}
+          }
+          return rtnNote
+        };
+      });  //close controller
+})(); //close function
