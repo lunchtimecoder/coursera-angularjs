6069726b95cfd31b7c2f9a45b6b5daa9f5acb21a
markpateman <email@lunchtime.com>
2016-11-24 23:07:58 +0000

    AngularJS Week 1 First code

--- week1/app.js
+++ week1/app.js
@@ -31,7 +31,8 @@
 
         //Test for empty string
         function checkForEmpties(item) {
-          return item.replace(/^\s+|\s+$/gm,'') == 0
+          return item.trim.length == 0
+          //return item.replace(/^\s+|\s+$/gm,'') == 0
         };
       });  //close MyController
 })(); //close function
