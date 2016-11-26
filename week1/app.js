c60eb80aef96f151703d708257af1dc570d633cd
markpateman <email@lunchtime.com>
2016-11-26 17:38:49 +0000

    v1 Week1

--- week1/app.js
+++ week1/app.js
@@ -22,13 +22,15 @@
               //Check for any empty items
               var numEmpty = itemsArray.filter(checkForEmpties).length
               var numItems = (itemsArray.length - numEmpty)
-              if(numEmpty == 0) { rtn.note = ""; }
+            if(numEmpty == 0) { rtn.note = ""; }
               else { rtn.note = "I do NOT consider an empty item valid food!"; }
             //Test if less than or equal to 3 items
             if( numItems <= 3) {
-              rtn = { "message":"Enjoy!", "colour":"boxColorBlue", "note":""};
+                rtn.message = "Enjoy!";
+                rtn.colour = "boxColorBlue";
             } else {
-              rtn = { "message":"Too Much!", "colour":"boxColorRed", "note":""};
+                rtn.message = "TooMuch!";
+                rtn.colour = "boxColorRed";
             }
           }
           return rtn;
