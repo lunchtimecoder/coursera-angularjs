79884b436a2a48b980aad03552595dd819acfaeb
markpateman <email@lunchtime.com>
2016-11-26 17:26:45 +0000

    v1 Week1

--- week1/app.js
+++ week1/app.js
@@ -1,37 +1,42 @@
 (function () {
   'use strict'
-  angular.module("MyApp", [])
-      .controller("MyController", function ($scope)
-      {
-        //init vars
-        $scope.items = "";  //will hold items to process
-        $scope.basket = { "message":"", "colour":"boxColorBlack", "note":"" }; //return object
-        $scope.displayMsg = function () {
-          $scope.basket = processItems($scope.items)
-        };
+  angular.module("LunchCheck", [])
+      .controller("LunchCheckController", LunchCheckController);
 
-        function processItems(inputItems) {
+  LunchCheckController.$inject = [$scope]
+
+  function LunchCheckController($scope) {
+      //init vars
+      $scope.items = "";  //will hold items to process
+      $scope.basket = {"message": "", "colour": "boxColorBlack", "note": ""}; //return object
+      $scope.displayMsg = function () {
+          $scope.basket = processItems($scope.items)
+      }
+  }
+  function processItems(inputItems) {
           var itemsArray = inputItems.split(",");
           var rtn = { "message":"Please enter data first", "colour":"boxColorBlack", "note":""};
 
           //Test if no items
           if(inputItems != "") {
+              //Check for any empty items
+              var numEmpty = itemsArray.filter(checkForEmpties).length
+              var numItems = (itemsArray.length - numEmpty)
+              if(numEmpty == 0) { rtn.note = ""; }
+              else { rtn.note = "I do NOT consider an empty item valid food!"; }
             //Test if less than or equal to 3 items
-            if(itemsArray.length <= 3) {
+            if( numItems <= 3) {
               rtn = { "message":"Enjoy!", "colour":"boxColorBlue", "note":""};
             } else {
               rtn = { "message":"Too Much!", "colour":"boxColorRed", "note":""};
             }
-            //Check for any empty items
-            if(itemsArray.filter(checkForEmpties).length == 0) { rtn.note = ""; }
-              else { rtn.note = "I do NOT consider an empty item valid food!"; }
           }
           return rtn;
-        }; //close processItems
+  } //close processItems
 
-        //Test for empty string
-        function checkForEmpties(item) {
-          return item.replace(/^\s+|\s+$/gm,'') == 0
-        };
-      });  //close MyController
+  //Test for empty string
+  function checkForEmpties(item) {
+      return item.replace(/^\s+|\s+$/gm,'') == 0
+  }
+
 })(); //close function
