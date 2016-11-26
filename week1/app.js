65f2dd30e8e1457cceb2469444871abca74234c6
markpateman <email@lunchtime.com>
2016-11-26 18:00:38 +0000

    v1 Week1

--- week1/app.js
+++ week1/app.js
@@ -15,7 +15,7 @@
   }
   function processItems(inputItems) {
           var itemsArray = inputItems.split(",");
-          var rtn = { "message":"Please enter data first", "colour":"border-color:black", "note":""};
+          var rtn = { "message":"Please enter data first", "colour":"'border-color':'black'", "note":""};
 
           //Test if no items
           if(inputItems != "") {
@@ -27,10 +27,10 @@
             //Test if less than or equal to 3 items
             if( numItems <= 3) {
                 rtn.message = "Enjoy!";
-                rtn.colour = "border-color:blue";
+                rtn.colour = "'border-color':'blue'";
             } else {
                 rtn.message = "TooMuch!";
-                rtn.colour = "border-color:red";
+                rtn.colour = "'border-color':'red'";
             }
           }
           return rtn;
