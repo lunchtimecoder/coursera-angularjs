087d09446939e0108fdc44fa4cc05ccb7571c4dc
markpateman <email@lunchtime.com>
2016-11-27 22:23:32 +0000

    v1 Week1

--- week1/app.js
+++ week1/app.js
@@ -7,16 +7,15 @@
 
   function LunchCheckController($scope) {
       //init vars
-      $scope.items = "";  //will hold items to process
-      $scope.basket = {"message": "", "colour": '"color:red"', "note": ""}; //return object
-      $scope.textColour = {color:'red'};
+      $scope.items = "";                  //will hold items to process
+      $scope.rtnValues = { message:"", textColour:{color:'black'}, note:"" };
       $scope.displayMsg = function () {
-          $scope.basket = processItems($scope.items)
+          $scope.rtnValues = processItems($scope.items)
       }
   }
   function processItems(inputItems) {
           var itemsArray = inputItems.split(",");
-          var rtn = { "message":"Please enter data first", "colour":"color:'red'", "note":""};
+          var rtn = { message:"Please enter data first", textColour:{color:'red'},note:"" };
 
           //Test if no items
           if(inputItems != "") {
@@ -26,13 +25,8 @@
             if(numEmpty == 0) { rtn.note = ""; }
               else { rtn.note = "I do NOT consider an empty item valid food!"; }
             //Test if less than or equal to 3 items
-            if( numItems <= 3) {
-                rtn.message = "Enjoy!";
-                rtn.colour = "color:'green'";
-            } else {
-                rtn.message = "TooMuch!";
-                rtn.colour = "color:'green'";
-            }
+            rtn.textColour = {color:'green'};
+            if( numItems <= 3) { rtn.message = "Enjoy!"; } else { rtn.message = "TooMuch!"; }
           }
           return rtn;
   } //close processItems
