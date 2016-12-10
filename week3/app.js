54c5da47c33ff429c8ab849df7c7fc6d20bcbb31
lunchtimecoder <mark@pateman.me>
2016-12-10 14:54:01 +0000

    Week3

--- week3/app.js
+++ week3/app.js
@@ -10,7 +10,19 @@
     function NarrowItDownController(MenuSearchService) {
       var NarrowCtl = this;
       NarrowCtl.button = function() {
-          return MenuSearchService.getMatchedMenusItems("test");
+          //return MenuSearchService.getMatchedMenusItems("test");
+          var rtnPromise = MenuSearchService.getMatchedMenusItems("test");
+          rtnPromise.then(function (result) {
+              // process result and only keep items that match
+              var foundItems = result.data;
+              console.log(foundItems);
+              // return processed items
+              //return foundItems;
+              return "djdjd";
+          }).catch(function (error) {
+              console.log("Not Happening! " + error)
+          });
+          return "test";
       }
     }
 
@@ -19,19 +31,29 @@
         var MenuSearch = this;
 
         MenuSearch.getMatchedMenusItems = function(searchTerm) {
-            $http({method: "GET", url: (SourcePath + "/menu_items.json") })
-                .then(function (result) {
-                // process result and only keep items that match
-                var foundItems = result.data;
-                console.log(foundItems);
-                // return processed items
-                //return foundItems;
-                    return searchTerm;
-            }).catch(function (error) {
-                console.log("Not Happening! " + error)
-            });
+            var response = $http({
+                method: "GET",
+                url: (SourcePath + "/menu_items.json")
+                });
+            return response;
         }
-
+//        MenuSearchService.$inject = ['$http','SourcePath'];
+//        function MenuSearchService($http,SourcePath) {
+//            var MenuSearch = this;
+//
+//            MenuSearch.getMatchedMenusItems = function(searchTerm) {
+//                $http({method: "GET", url: (SourcePath + "/menu_items.json") })
+//                    .then(function (result) {
+//                        // process result and only keep items that match
+//                        var foundItems = result.data;
+//                        console.log(foundItems);
+//                        // return processed items
+//                       //return foundItems;
+//                        return searchTerm;
+//                    }).catch(function (error) {
+//                    console.log("Not Happening! " + error)
+//                });
+//            }
 
     } //End of MenuSearchService
 
