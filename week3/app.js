da3c0a23cae54e85a6da763c4e0f1fbb5b3683c7
lunchtimecoder <mark@pateman.me>
2016-12-10 12:51:22 +0000

    Week3

--- week3/app.js
+++ week3/app.js
@@ -18,12 +18,14 @@
         var MenuSearch = this;
 
         MenuSearch.getMatchedMenusItems = function(searchTerm) {
-            return $http({url: "https://davids-restaurant.herokuapp.com/menu_items.json"}).then(function (result) {
+            return $http({url: "https://davids-restaurant.herokuapp.com/menu_items.json"})
+                .then(function (result) {
                 // process result and only keep items that match
                 var foundItems = result.data;
                 console.log(foundItems);
                 // return processed items
-                return foundItems;
+                //return foundItems;
+                    return "place holder";
             }).catch(function (error) {
                 console.log("Not Happening! " + error)
             });
