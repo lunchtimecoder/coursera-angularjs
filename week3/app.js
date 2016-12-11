337467a2169e815aad60c37db8082e98b419cf82
lunchtimecoder <mark@pateman.me>
2016-12-11 10:21:02 +0000

    Week3

--- week3/app.js
+++ week3/app.js
@@ -30,31 +30,20 @@
     function MenuSearchService($http,SourcePath) {
         var MenuSearch = this;
 //"/menu_items.json"
-        MenuSearch.getMatchedMenusItems = function(searchTerm) {
-            var response = $http({
-                method: "GET",
-                url: (SourcePath + "/menu_items.json")
+
+         MenuSearch.getMatchedMenusItems = function(searchTerm) {
+             $http({method: "GET", url: (SourcePath + "/menu_items.json") })
+                    .then(function (result) {
+                        // process result and only keep items that match
+                        var foundItems = result.data.menu_items;
+                        console.log(foundItems);
+                        // return processed items
+                       //return foundItems;
+                        return searchTerm;
+                    }).catch(function (error) {
+                    console.log("Not Happening! " + error)
                 });
-            console.log(response);
-            return "no";
-        }
-//        MenuSearchService.$inject = ['$http','SourcePath'];
-//        function MenuSearchService($http,SourcePath) {
-//            var MenuSearch = this;
-//
-//            MenuSearch.getMatchedMenusItems = function(searchTerm) {
-//                $http({method: "GET", url: (SourcePath + "/menu_items.json") })
-//                    .then(function (result) {
-//                        // process result and only keep items that match
-//                        var foundItems = result.data;
-//                        console.log(foundItems);
-//                        // return processed items
-//                       //return foundItems;
-//                        return searchTerm;
-//                    }).catch(function (error) {
-//                    console.log("Not Happening! " + error)
-//                });
-//            }
+            }
 
     } //End of MenuSearchService
 
