4cfecdd94e9c40d1255ef9b4e1bb3dfe27a905d1
lunchtimecoder <mark@pateman.me>
2016-12-24 11:48:02 +0000

    week5

--- week5/src/public/signup/signup.controller.js
+++ week5/src/public/signup/signup.controller.js
@@ -10,9 +10,12 @@
         signupCtrl.submit = function () {
             signupCtrl.notFound = false;
             signupCtrl.completed = true;
+
+            signupCtrl.user.fav = signupCtrl.user.fav.toUpperCase();
             var r = menuItems.menu_items.filter(function (i){
                 return i.short_name === signupCtrl.user.fav;
             });
+
             SignupService.saveUser(signupCtrl.user)
             if( r.length === 0 ) {
                 signupCtrl.notFound = true;
